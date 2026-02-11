3309625299010d183386c63f9fe0fa3f724c8036
Program <67320703+Program132@users.noreply.github.com>
2026-02-11 19:33:38 +0100

    Expressions Nodes + enum for expr type

---
+++ src/parser/ASTNodes/Expression.cpp
@@ -0,0 +1,61 @@
+#include "Expression.h"
+
+Expression::Expression() { this->type = EMPTY_EXPR; }
+Expression::Expression(ExpressionType type) { this->type = type; }
+
+ExpressionType Expression::getType() const { return type; }
+
+BinaryExpression::BinaryExpression(Expression left, Token op, Expression right)
+    : Expression(BINARY), left(left), op(op), right(right) {}
+
+Expression BinaryExpression::getLeft() const { return left; }
+Token BinaryExpression::getOp() const { return op; }
+Expression BinaryExpression::getRight() const { return right; }
+
+UnaryExpression::UnaryExpression(Token op, Expression right)
+    : Expression(UNARY), op(op), right(right) {}
+
+Token UnaryExpression::getOp() const { return op; }
+Expression UnaryExpression::getRight() const { return right; }
+
+LiteralExpression::LiteralExpression(Token value)
+    : Expression(LITERAL), value(value) {}
+
+Token LiteralExpression::getValue() const { return value; }
+
+GroupingExpression::GroupingExpression(Expression expression)
+    : Expression(GROUPING), expression(expression) {}
+
+Expression GroupingExpression::getExpression() const { return expression; }
+
+VariableExpression::VariableExpression(Token name)
+    : Expression(VARIABLE), name(name) {}
+
+Token VariableExpression::getName() const { return name; }
+
+AssignExpression::AssignExpression(Token name, Expression value)
+    : Expression(ASSIGN), name(name), value(value) {}
+
+Token AssignExpression::getName() const { return name; }
+Expression AssignExpression::getValue() const { return value; }
+
+CallExpression::CallExpression(Expression callee, Token paren,
+                               List<Expression> arguments)
+    : Expression(CALL), callee(callee), paren(paren), arguments(arguments) {}
+
+Expression CallExpression::getCallee() const { return callee; }
+Token CallExpression::getParen() const { return paren; }
+List<Expression> CallExpression::getArguments() const { return arguments; }
+
+GetExpression::GetExpression(Expression object, Token name)
+    : Expression(GET), object(object), name(name) {}
+
+Expression GetExpression::getObject() const { return object; }
+Token GetExpression::getName() const { return name; }
+
+SetExpression::SetExpression(Expression object, Token name, Expression value)
+    : Expression(SET), object(object), name(name), value(value) {}
+
+Expression SetExpression::getObject() const { return object; }
+Token SetExpression::getName() const { return name; }
+Expression SetExpression::getValue() const { return value; }
---
+++ src/parser/ASTNodes/Expression.h
@@ -0,0 +1,139 @@
+#ifndef EXPRESSION_H
+#define EXPRESSION_H
+
+#include "../../lexer/Token/Token.h"
+#include "../../utils/List.h"
+
+enum ExpressionType {
+  EMPTY_EXPR,
+  BINARY,
+  UNARY,
+  LITERAL,
+  GROUPING,
+  VARIABLE,
+  ASSIGN,
+  CALL,
+  GET,
+  SET
+};
+
+class Expression {
+private:
+  ExpressionType type;
+
+public:
+  Expression();
+  Expression(ExpressionType type);
+
+  ExpressionType getType() const;
+};
+
+class BinaryExpression : public Expression {
+private:
+  Expression left;
+  Token op;
+  Expression right;
+
+public:
+  BinaryExpression(Expression left, Token op, Expression right);
+
+  Expression getLeft() const;
+  Token getOp() const;
+  Expression getRight() const;
+};
+
+class UnaryExpression : public Expression {
+private:
+  Token op;
+  Expression right;
+
+public:
+  UnaryExpression(Token op, Expression right);
+
+  Token getOp() const;
+  Expression getRight() const;
+};
+
+class LiteralExpression : public Expression {
+private:
+  Token value;
+
+public:
+  LiteralExpression(Token value);
+
+  Token getValue() const;
+};
+
+class GroupingExpression : public Expression {
+private:
+  Expression expression;
+
+public:
+  GroupingExpression(Expression expression);
+
+  Expression getExpression() const;
+};
+
+class VariableExpression : public Expression {
+private:
+  Token name;
+
+public:
+  VariableExpression(Token name);
+
+  Token getName() const;
+};
+
+class AssignExpression : public Expression {
+private:
+  Token name;
+  Expression value;
+
+public:
+  AssignExpression(Token name, Expression value);
+
+  Token getName() const;
+  Expression getValue() const;
+};
+
+class CallExpression : public Expression {
+private:
+  Expression callee;
+  Token paren;
+  List<Expression> arguments;
+
+public:
+  CallExpression(Expression callee, Token paren, List<Expression> arguments);
+
+  Expression getCallee() const;
+  Token getParen() const;
+  List<Expression> getArguments() const;
+};
+
+class GetExpression : public Expression {
+private:
+  Expression object;
+  Token name;
+
+public:
+  GetExpression(Expression object, Token name);
+
+  Expression getObject() const;
+  Token getName() const;
+};
+
+class SetExpression : public Expression {
+private:
+  Expression object;
+  Token name;
+  Expression value;
+
+public:
+  SetExpression(Expression object, Token name, Expression value);
+
+  Expression getObject() const;
+  Token getName() const;
+  Expression getValue() const;
+};
+
+#endif // EXPRESSION_H
