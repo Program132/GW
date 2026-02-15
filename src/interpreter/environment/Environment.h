f29e6d8e39182de3b7e29b3e262910d9681275d6
Program <67320703+Program132@users.noreply.github.com>
2026-02-16 00:44:16 +0100

    Added methods about variable to Environment class

--- src/interpreter/environment/Environment.h
+++ src/interpreter/environment/Environment.h
@@ -1,10 +1,69 @@
 #ifndef ENVIRONMENT_H
 #define ENVIRONMENT_H
 
+#include "../value/Value.h"
+#include <map>
+#include <string>
+
 class Environment {
 private:
+  Environment *parent;
+  std::map<std::string, Value> variables;
+  std::map<std::string, std::string> variableTypes;
+
 public:
-  Environment() {}
+  Environment() : parent(nullptr) {}
+  Environment(Environment *parent) : parent(parent) {}
+
+  void addVariable(const std::string &name, const Value &value,
+                   const std::string &type) {
+    variables[name] = value;
+    variableTypes[name] = type;
+  }
+
+  bool variableExist(const std::string &name) {
+    if (variables.find(name) != variables.end()) {
+      return true;
+    }
+    if (parent != nullptr) {
+      return parent->variableExist(name);
+    }
+    return false;
+  }
+
+  Value getVariableValue(const std::string &name) {
+    if (variables.find(name) != variables.end()) {
+      return variables[name];
+    }
+    if (parent != nullptr) {
+      return parent->getVariableValue(name);
+    }
+    return Value(); // Returns Null value
+  }
+
+  std::string getVariableType(const std::string &name) {
+    if (variableTypes.find(name) != variableTypes.end()) {
+      return variableTypes[name];
+    }
+    if (parent != nullptr) {
+      return parent->getVariableType(name);
+    }
+    return "Unknown";
+  }
+
+  void updateVariable(const std::string &name, const Value &value,
+                      const std::string &type = "") {
+    if (variables.find(name) != variables.end()) {
+      variables[name] = value;
+      if (!type.empty()) {
+        variableTypes[name] = type;
+      }
+      return;
+    }
+    if (parent != nullptr) {
+      parent->updateVariable(name, value, type);
+    }
+  }
 };
 
 #endif
