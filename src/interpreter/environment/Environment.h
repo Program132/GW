abe9be1bbde4eff7734bc753a31f6ddff975c17a
Program <67320703+Program132@users.noreply.github.com>
2026-02-16 21:44:18 +0100

    Added struct management in env

--- src/interpreter/environment/Environment.h
+++ src/interpreter/environment/Environment.h
@@ -1,11 +1,11 @@
 #ifndef ENVIRONMENT_H
 #define ENVIRONMENT_H
 
+#include "../../parser/ASTNodes/Statement.h"
 #include "../Interpreter/Function.h"
 #include "../value/Value.h"
 #include <map>
 #include <string>
-
 
 class Environment {
 private:
@@ -13,10 +13,35 @@ private:
   std::map<std::string, Value> variables;
   std::map<std::string, std::string> variableTypes;
   std::map<std::string, Function> functions;
+  std::map<std::string, StructDeclarationStatement *> structs;
 
 public:
   Environment() : parent(nullptr) {}
   Environment(Environment *parent) : parent(parent) {}
+
+  void addStruct(const std::string &name, StructDeclarationStatement *stmt) {
+    structs[name] = stmt;
+  }
+
+  bool structExist(const std::string &name) {
+    if (structs.find(name) != structs.end()) {
+      return true;
+    }
+    if (parent != nullptr) {
+      return parent->structExist(name);
+    }
+    return false;
+  }
+
+  StructDeclarationStatement *getStruct(const std::string &name) {
+    if (structs.find(name) != structs.end()) {
+      return structs[name];
+    }
+    if (parent != nullptr) {
+      return parent->getStruct(name);
+    }
+    return nullptr;
+  }
 
   void addVariable(const std::string &name, const Value &value,
                    const std::string &type) {
