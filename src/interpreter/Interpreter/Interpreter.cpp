27b947b2e59db47754a05aa5e2d16d35e685ea5a
Program <67320703+Program132@users.noreply.github.com>
2026-02-16 19:09:33 +0100

    Implemented function declaration + call + returns

--- src/interpreter/Interpreter/Interpreter.cpp
+++ src/interpreter/Interpreter/Interpreter.cpp
@@ -1,13 +1,24 @@
 #include "Interpreter.h"
+#include "ReturnException.h"
 #include <iostream>
 
-Interpreter::Interpreter() {}
+Interpreter::Interpreter() { environment = new Environment(); }
 
-Interpreter::Interpreter(List<Statement> statements) : statements(statements) {}
+Interpreter::Interpreter(List<Statement *> statements)
+    : statements(statements) {
+  environment = new Environment();
+}
 
-List<Statement> Interpreter::getStatements() const { return statements; }
+Interpreter::~Interpreter() {
+  delete environment;
+  for (int i = 0; i < statements.size(); i++) {
+    delete statements.get(i);
+  }
+}
 
-Environment Interpreter::getEnvironment() const { return environment; }
+List<Statement *> Interpreter::getStatements() const { return statements; }
+
+Environment *Interpreter::getEnvironment() const { return environment; }
 
 void Interpreter::interpret() {
   for (int i = 0; i < statements.size(); i++) {
@@ -15,15 +26,15 @@ void Interpreter::interpret() {
   }
 }
 
-void Interpreter::execute(Statement statement) {
-  switch (statement.getType()) {
+void Interpreter::execute(Statement *statement) {
+  switch (statement->getType()) {
   case PRINT_STATEMENT: {
-    if (statement.getExpression().getType() == EMPTY_EXPR) {
+    if (statement->getExpression()->getType() == EMPTY_EXPR) {
       return;
     }
 
-    Value evaluation = evaluate(statement.getExpression());
-    ExpressionType exprType = statement.getExpression().getType();
+    Value evaluation = evaluate(statement->getExpression());
+    ExpressionType exprType = statement->getExpression()->getType();
 
     if (exprType == ASSIGN || exprType == SET) {
       std::cerr
@@ -37,13 +48,13 @@ void Interpreter::execute(Statement statement) {
     break;
   }
   case PRINTLN_STATEMENT: {
-    if (statement.getExpression().getType() == EMPTY_EXPR) {
+    if (statement->getExpression()->getType() == EMPTY_EXPR) {
       std::cout << std::endl;
       return;
     }
 
-    Value evaluation = evaluate(statement.getExpression());
-    ExpressionType exprType = statement.getExpression().getType();
+    Value evaluation = evaluate(statement->getExpression());
+    ExpressionType exprType = statement->getExpression()->getType();
 
     if (exprType == ASSIGN || exprType == SET) {
       std::cerr
@@ -57,24 +68,49 @@ void Interpreter::execute(Statement statement) {
     break;
   }
   case VAR_DECLARATION: {
-    std::string varName = statement.getToken().getValue();
+    std::string varName = statement->getToken().getValue();
     Value value; // Null
 
-    if (statement.getExpression().getType() != EMPTY_EXPR) {
-      value = evaluate(statement.getExpression());
+    if (statement->getExpression() != nullptr &&
+        statement->getExpression()->getType() != EMPTY_EXPR) {
+      value = evaluate(statement->getExpression());
     }
 
-    if (environment.variableExist(varName)) {
+    if (environment->variableExist(varName)) {
       std::cerr << "[ERROR] Variable '" << varName << "' already defined"
                 << std::endl;
       return;
     }
 
-    environment.addVariable(varName, value, "Unknown");
+    environment->addVariable(varName, value, "Unknown");
     break;
   }
+  case BLOCK: {
+    BlockStatement *stmt = (BlockStatement *)statement;
+    for (int i = 0; i < stmt->getStatements().size(); i++) {
+      execute(stmt->getStatements().get(i));
+    }
+    break;
+  }
+  case FUNCTION_DECLARATION: {
+    FunctionDeclarationStatement *stmt =
+        (FunctionDeclarationStatement *)statement;
+    std::string funcName = stmt->getToken().getValue();
+    Function func(funcName, stmt->getParams(), BlockStatement(stmt->getBody()),
+                  stmt->getReturnType());
+    environment->addFunction(funcName, func);
+    break;
+  }
+  case RETURN: {
+    Value value;
+    if (statement->getExpression() != nullptr &&
+        statement->getExpression()->getType() != EMPTY_EXPR) {
+      value = evaluate(statement->getExpression());
+    }
+    throw ReturnException(value);
+  }
   case EXPRESSION_STATEMENT: {
-    evaluate(statement.getExpression());
+    evaluate(statement->getExpression());
     break;
   }
   default:
@@ -82,78 +118,82 @@ void Interpreter::execute(Statement statement) {
   }
 }
 
-Value Interpreter::evaluate(Expression expression) {
-  switch (expression.getType()) {
+Value Interpreter::evaluate(Expression *expression) {
+  switch (expression->getType()) {
   case LITERAL: {
-    return tokenToValue(expression.getToken());
+    return tokenToValue(expression->getToken());
   }
   case VARIABLE: {
-    std::string varName = expression.getToken().getValue();
-    if (!environment.variableExist(varName)) {
+    std::string varName = expression->getToken().getValue();
+    if (!environment->variableExist(varName)) {
       throw std::runtime_error("Undefined variable '" + varName + "'");
     }
-    return environment.getVariableValue(varName);
+    return environment->getVariableValue(varName);
   }
   case ASSIGN: {
-    std::string varName = expression.getToken().getValue();
-    Value value = evaluate(expression.getChildren().get(0));
+    std::string varName = expression->getToken().getValue();
+    Value value = evaluate(expression->getChildren().get(0));
 
-    if (!environment.variableExist(varName)) {
+    if (!environment->variableExist(varName)) {
       throw std::runtime_error("Undefined variable '" + varName + "'");
     }
 
-    environment.updateVariable(varName, value);
+    environment->updateVariable(varName, value);
     return value;
   }
   case GROUPING: {
-    return evaluate(expression.getChildren().get(0));
+    return evaluate(expression->getChildren().get(0));
   }
   case UNARY: {
-    Value right = evaluate(expression.getChildren().get(0));
-    std::string op = expression.getToken().getValue();
+    Value right = evaluate(expression->getChildren().get(0));
+    std::string op = expression->getToken().getValue();
 
     if (op == "-") {
-      return -right;
-    } else if (op == "+") {
-      return +right;
+      if (right.isNumber())
+        return Value(-right.asDouble());
+      else
+        throw std::runtime_error("Unary minus requires number operand");
     } else if (op == "!") {
-      if (!right.isBool()) {
-        throw std::runtime_error(
-            "Logical NOT operator requires boolean operand");
-      }
       return Value(!right.asBool());
     }
-    return right;
+    throw std::runtime_error("Unknown unary operator: " + op);
   }
   case BINARY: {
-    Value left = evaluate(expression.getChildren().get(0));
-    Value right = evaluate(expression.getChildren().get(1));
-    std::string op = expression.getToken().getValue();
+    Value left = evaluate(expression->getChildren().get(0));
+    Value right = evaluate(expression->getChildren().get(1));
+    std::string op = expression->getToken().getValue();
 
     if (op == "+") {
-      return left + right;
+      if (left.isNumber() && right.isNumber())
+        return Value(left.asDouble() + right.asDouble());
+      else if (left.isString() && right.isString())
+        return Value(left.asString() + right.asString());
+      else if (left.isString())
+        return Value(left.asString() + right.toString());
+      else if (right.isString())
+        return Value(left.toString() + right.asString());
+      else
+        throw std::runtime_error("Invalid operands for '+' operator");
     } else if (op == "-") {
-      return left - right;
+      return Value(left.asDouble() - right.asDouble());
     } else if (op == "*") {
-      return left * right;
+      return Value(left.asDouble() * right.asDouble());
     } else if (op == "/") {
-      return left / right;
-    } else if (op == "%") {
-      return left % right;
-    } else if (op == "^") {
-      return left.pow(right);
+      if (right.asDouble() == 0)
+        throw std::runtime_error("Division by zero");
+      return Value(left.asDouble() / right.asDouble());
     } else if (op == "==") {
       return Value(left == right);
-    } else if (op == "~=" || op == "!=") {
-      return Value(left != right);
+    } else if (op == "!=") {
+      return Value(!(left == right));
     } else if (op == "<") {
-      return Value(left < right);
+      return Value(left.asDouble() < right.asDouble());
     } else if (op == ">") {
-      return Value(left > right);
+      return Value(left.asDouble() > right.asDouble());
     } else if (op == "<=") {
-      return Value(left <= right);
+      return Value(left.asDouble() <= right.asDouble());
     } else if (op == ">=") {
-      return Value(left >= right);
+      return Value(left.asDouble() >= right.asDouble());
     } else if (op == "&" || op == "&&") {
       if (!left.isBool() || !right.isBool()) {
         throw std::runtime_error(
@@ -171,7 +211,57 @@ Value Interpreter::evaluate(Expression expression) {
     throw std::runtime_error("Unknown binary operator: " + op);
   }
   case CALL: {
-    return Value("[Function Call]");
+    Expression *calleeExpr = expression->getChildren().get(0);
+    if (calleeExpr->getType() != VARIABLE) {
+      throw std::runtime_error("Only direct function calls are supported");
+    }
+
+    std::string funcName = calleeExpr->getToken().getValue();
+    if (!environment->functionExist(funcName)) {
+      throw std::runtime_error("Function '" + funcName + "' not defined");
+    }
+
+    Function func = environment->getFunction(funcName);
+    List<Expression *> argExprs =
+        ((CallExpression *)expression)->getArguments();
+
+    if (argExprs.size() != func.params.size()) {
+      throw std::runtime_error("Function '" + funcName + "' expects " +
+                               std::to_string(func.params.size()) +
+                               " arguments but got " +
+                               std::to_string(argExprs.size()));
+    }
+
+    List<Value> evaluatedArgs;
+    for (int i = 0; i < argExprs.size(); i++) {
+      evaluatedArgs.append(evaluate(argExprs.get(i)));
+    }
+
+    Environment *funcEnv = new Environment(environment);
+
+    for (int i = 0; i < evaluatedArgs.size(); i++) {
+      Token paramNameToken = func.params.get(i).get(1);
+      Token paramTypeToken = func.params.get(i).get(0);
+      funcEnv->addVariable(paramNameToken.getValue(), evaluatedArgs.get(i),
+                           paramTypeToken.getValue());
+    }
+
+    Environment *previousEnv = environment;
+    environment = funcEnv;
+
+    Value returnValue;
+    try {
+      for (int i = 0; i < func.body.getStatements().size(); i++) {
+        execute(func.body.getStatements().get(i));
+      }
+    } catch (const ReturnException &e) {
+      returnValue = e.value;
+    }
+
+    environment = previousEnv;
+    delete funcEnv;
+
+    return returnValue;
   }
   case GET: {
     return Value("[Get Property]");
