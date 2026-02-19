0e9529354c2a3213a6102e3e1a8e887cb22b11fa
Program <67320703+Program132@users.noreply.github.com>
2026-02-19 19:45:59 +0100

    Added __nativeGetArg & __nativeGetArgCount to System (native, C++)

--- src/native/System.h
+++ src/native/System.h
@@ -62,4 +62,32 @@ inline Value __nativeExit(const std::vector<Value> &args) {
   return Value();
 }
 
+// Global storage for command line arguments
+static std::vector<std::string> g_cmdArgs;
+
+inline void GW_setArgs(int argc, char *argv[], int startIndex) {
+  g_cmdArgs.clear();
+  for (int i = startIndex; i < argc; i++) {
+    g_cmdArgs.push_back(argv[i]);
+  }
+}
+
+inline Value __nativeGetArgCount(const std::vector<Value> &args) {
+  if (args.size() != 0) {
+    throw std::runtime_error("[GW NATIVE] getArgCount() takes 0 arguments");
+  }
+  return Value((int)g_cmdArgs.size());
+}
+
+inline Value __nativeGetArg(const std::vector<Value> &args) {
+  if (args.size() != 1) {
+    throw std::runtime_error("[GW NATIVE] getArg(index) takes 1 argument");
+  }
+  int index = args[0].asInt();
+  if (index < 0 || index >= (int)g_cmdArgs.size()) {
+    throw std::runtime_error("[GW NATIVE] Argument index out of bounds");
+  }
+  return Value(g_cmdArgs[index]);
+}
+
 #endif // SYSTEM_H
