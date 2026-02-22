3d75466b8141d1f83b31d4779625ea43a3329fbe
Program <67320703+Program132@users.noreply.github.com>
2026-02-22 15:50:20 +0100

    Added native functions to String.h

--- src/native/String.h
+++ src/native/String.h
@@ -23,7 +23,36 @@ inline Value __nativeStringAt(const std::vector<Value> &args) {
     throw std::runtime_error(
         "[GW NATIVE] stringAt() takes a string and an int");
   }
-  return Value(args[0].asString()[args[1].asInt()]);
+  char c = args[0].asString()[args[1].asInt()];
+  return Value(std::string(1, c));
+}
+
+inline Value __nativeStringToInt(const std::vector<Value> &args) {
+  if (args.size() != 1) {
+    throw std::runtime_error("[GW NATIVE] stringToInt() takes 1 argument");
+  }
+  if (!args[0].isString()) {
+    throw std::runtime_error("[GW NATIVE] stringToInt() takes a string");
+  }
+  try {
+    return Value(std::stoi(args[0].asString()));
+  } catch (...) {
+    return Value(0);
+  }
+}
+
+inline Value __nativeStringToDouble(const std::vector<Value> &args) {
+  if (args.size() != 1) {
+    throw std::runtime_error("[GW NATIVE] stringToDouble() takes 1 argument");
+  }
+  if (!args[0].isString()) {
+    throw std::runtime_error("[GW NATIVE] stringToDouble() takes a string");
+  }
+  try {
+    return Value(std::stod(args[0].asString()));
+  } catch (...) {
+    return Value(0.0);
+  }
 }
 
 #endif
